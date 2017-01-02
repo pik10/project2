d6b2296bab0daf4351f20647931ddfff9913ba5e
Peter Gniewek <petergniewek@peters-mbp.frontier.com>
2017-01-02 13:21:21 -0500

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Peter Gniewek on 1/2/17.
+//  Copyright © 2017 Peter Gniewek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
